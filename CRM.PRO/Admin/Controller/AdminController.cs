0de41cacc1f0a619dac00ca33ef05480bb738dc3
Trung The Master <tng333@gmail.com>
2018-04-08 16:12:07 -0400

    Add project files.

---
+++ CRM.PRO/Admin/Controller/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.PRO.Admin
+{
+    [Produces("application/json")]
+    [Route("api/Admin")]
+    public class AdminController : Controller
+    {
+
+    }
+}
